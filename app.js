ac80c75d607a72d8aecc4bd6163cafa8a5e7202b
David Miller <hawky454@gmail.com>
2018-12-19 16:44:48 -0600

    almost lost everything couldnt get nodemon to work had to update a few dependencies

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const cellar = require('./api/cellar');
 const pipes = require('./api/pipes');
 const users = require('./api/users');
@@ -17,11 +16,13 @@ const users = require('./api/users');
 
 //! not sure if this is needed
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
-
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,35 +32,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //! dont think I need these now that I've installed body-parser?
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
-app.use(cors({
-  credentials: true
-}));
-
-
-
-
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(
+  cors({
+    credentials: true
+  })
+);
 
 // app.use('/auth', auth);
 app.use('/api/cellar', cellar);
 app.use('/api/pipes', pipes);
 app.use('/api/users', users);
 
-
-
-
-
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
